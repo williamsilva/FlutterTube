873dfac579ad916ce7a689a605a6b6c2115460fd
DESKTOP-WS\William <wyllyambmg@gmail.com>
2019-02-21 22:29:09 -0300

    projeto final

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,32 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertube/api.dart';
+import 'package:fluttertube/blocs/favorite_bloc.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
 import 'package:fluttertube/screens/home.dart';
 
-void main() => runApp(MyApp());
+void main() {
+
+  Api api = Api();
+  api.search("eletro");
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Home(),
+    return BlocProvider(
+      bloc: VideosBloc(),
+      child: BlocProvider(
+        bloc: FavoriteBloc(),
+          child: MaterialApp(
+            title: 'FlutterTube',
+            debugShowCheckedModeBanner: false,
+            home: Home(),
+          ),
+      )
     );
   }
 }
